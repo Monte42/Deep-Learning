9f3fe96d95c21d4bb0958744c4effd9f25aa00ad
Monte DuMond <gdumond42@gmail.com>
2023-11-06 20:39:39 -0500

    starting convolution neural network

---
+++ CNN/cnn.py
@@ -0,0 +1,48 @@
+# IMPORTS
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
+# =================================
+# BUILD CONVOLUTION NEURAL NETWORK
+# =================================
+# INITIALIZE CONVOLUTION NEURAL NETWORK
+
+
+
+# CONVOLUTION
+
+
+
+# POOLING
+
+
+
+# ADD SECOND CONVOLUTION
+
+
+
+# FLATTENING
+
+
+
+# FULL CONNECTION WITH ANN
+
+
+
+# ADD OUTPUT LAYER
+
+
+
+
+
+# ==========
+# TRAIN CNN
+# ==========
+
+
+
+# =========
+# TEST CNN
+# =========
