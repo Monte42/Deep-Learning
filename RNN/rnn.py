d3fadb82e81f2e555e18afbc0be924fda0e06f64
Monte DuMond <gdumond42@gmail.com>
2023-11-10 11:10:19 -0500

    import data/scale data/set timesteps/shape data => 3D numpy array

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -6,7 +6,25 @@ import pandas as pd
 # ====================
 # DATA PRE-PROCESSING
 # ====================
+# Import Train Set
+dataset_train = pd.read_csv('datasets/Google_Stock_Price_Train.csv')
+training_set = dataset_train.iloc[:,1:2].values
 
+# Feature Scaling
+sc = MinMaxScaler(feature_range=(0,1), copy=True) # these are default and dont need to add
+scaled_train_set = sc.fit_transform(training_set)
+
+# NEW - Setting Number of Time Steps
+x_train = []
+y_train = []
+for i in range(60,len(scaled_train_set)):
+    x_train.append(scaled_train_set[i-60:i, 0])
+    y_train.append(scaled_train_set[i, 0])
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+# Reshaping
+x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1], 1)) # (batch_size,time_steps,predictors)
+# batch_size = # of rows, time_steps = # of cols, predictors = # of values in cell
 
 
 
