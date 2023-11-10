c5f490bd52afa366caf54d7318878f8b15d1d649
Monte DuMond <gdumond42@gmail.com>
2023-11-10 09:54:24 -0500

    Set up Steps for RNN in rnn.py

---
+++ RNN/rnn.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+
+# ====================
+# DATA PRE-PROCESSING
+# ====================
+
+
+
+
+# =================
+# BUILDING THE RNN
+# =================
+
+
+
+
+# =================
+# TRAINING THE RNN
+# =================
+
+
+
+
+# ===============================================
+# MAKING PREDICTIONS AND VISUALIZING THE RESULTS
+# ===============================================
