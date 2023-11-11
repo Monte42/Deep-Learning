f1a8679a0d2e06b58425c4e15736d47df3a07360
Monte DuMond <gdumond42@gmail.com>
2023-11-11 14:47:12 -0500

    Run Test / Make Predictions / Show Graph

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -80,8 +80,8 @@ inputs = inputs.reshape(-1,1)
 inputs = sc.transform(inputs)
 
 x_test = []
-for i in range(60,80):
-    x_test.append(inputs[i-60:i, 0])
+for i in range(60,80): # Get last 20 days ( 60th day - 80th )
+    x_test.append(inputs[i-60:i, 0]) 
 x_test = np.array(x_test)
 
 x_test = np.reshape(x_test, (x_test.shape[0],x_test.shape[1], 1))
