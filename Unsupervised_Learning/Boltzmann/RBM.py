9bac667c1e3727399396b46216caa4442eced549
Monte DuMond <gdumond42@gmail.com>
2023-11-14 07:54:40 -0500

    fixed enviroment problem

---
+++ Unsupervised_Learning/Boltzmann/RBM.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd 
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from torch.autograd import Variable
+
+movies = pd.read_csv('ml-1m/movies.dat', sep='::', header=None, engine='python', encoding='latin-1')
